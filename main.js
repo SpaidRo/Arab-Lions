fea5a5511633d990848fdc95c3779afdcd8e9adc
SpaidRo <84509825+SpaidRo@users.noreply.github.com>
2022-05-01 13:39:36 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,235 @@
+/*   buttons contact */
+function funcspaidro() {
+    alert("Connect with him on Discord: SpaidRō .#4140")
+}
+
+function funloven() {
+    alert("Connect with him on Discord: Løven .#0798")
+}
+
+function funkarlos() {
+    alert("Connect with him on Discord: 𝖊𝖑_𝕶𝖆𝕽𝕷𝖔𝖘 .#0709")
+}
+
+function funOREO() {
+    alert("Connect with him on Discord: set.#4030")
+}
+/*============================*/
+function funcdiscord() {
+    document.getElementById("divprofile1").style.display = "none";
+    document.getElementById("divprofile2").style.display = "none";
+    document.getElementById("imgdevlopper").style.display = "none";
+    document.getElementById("profilename").style.display = "none";
+    document.getElementById("moroccoflag").style.display = "none";
+    document.getElementById("hobies").style.display = "none";
+    document.getElementById("TotalServers").style.display = "none";
+    document.getElementById("TotalServers2").style.display = "none";
+    document.getElementById("TotalMembers").style.display = "none";
+    document.getElementById("TotalMembers2").style.display = "none";
+    document.getElementById("texthome").style.display = "none";
+    document.getElementById("textcop").style.display = "none";
+    document.getElementById("imgdis2").style.display = "block";
+    document.getElementById("buttondis22").style.display = "block";
+    /*spaidro block*/
+    document.getElementById("divspaidro").style.display = "none";
+    document.getElementById("imgspaidro").style.display = "none";
+    document.getElementById("imoowner").style.display = "none";
+    document.getElementById("spaibut").style.display = "none";
+    /*Loven block*/
+    document.getElementById("divloven").style.display = "none";
+    document.getElementById("imgloven").style.display = "none";
+    document.getElementById("imcoowner").style.display = "none";
+    document.getElementById("lovenbut").style.display = "none";
+    /*karlos block*/
+    document.getElementById("divkarlos").style.display = "none";
+    document.getElementById("imgkarlos").style.display = "none";
+    document.getElementById("imcoowner2").style.display = "none";
+    document.getElementById("karlosbut").style.display = "none";
+    /*oreo block*/
+    document.getElementById("divoreo").style.display = "none";
+    document.getElementById("imgoreo").style.display = "none";
+    document.getElementById("imcoowner3").style.display = "none";
+    document.getElementById("oreobut").style.display = "none";
+    document.getElementById("titleinformation").style.display = "none";
+    document.getElementById("informationtext").style.display = "none";
+
+}
+
+function funadmins() {
+    document.getElementById("texthome").style.display = "none";
+    document.getElementById("textcop").style.display = "none";
+    document.getElementById("imgdis2").style.display = "none";
+    document.getElementById("buttondis22").style.display = "none";
+    /*spaidro block*/
+    document.getElementById("divspaidro").style.display = "block";
+    document.getElementById("imgspaidro").style.display = "block";
+    document.getElementById("imoowner").style.display = "block";
+    document.getElementById("spaibut").style.display = "block";
+    /*Loven block*/
+    document.getElementById("divloven").style.display = "block";
+    document.getElementById("imgloven").style.display = "block";
+    document.getElementById("imcoowner").style.display = "block";
+    document.getElementById("lovenbut").style.display = "block";
+    /*karlos block*/
+    document.getElementById("divkarlos").style.display = "block";
+    document.getElementById("imgkarlos").style.display = "block";
+    document.getElementById("imcoowner2").style.display = "block";
+    document.getElementById("karlosbut").style.display = "block";
+    /*oreo block*/
+    document.getElementById("divoreo").style.display = "block";
+    document.getElementById("imgoreo").style.display = "block";
+    document.getElementById("imcoowner3").style.display = "block";
+    document.getElementById("oreobut").style.display = "block";
+    document.getElementById("titleinformation").style.display = "none";
+    document.getElementById("informationtext").style.display = "none";
+    document.getElementById("divprofile1").style.display = "none";
+    document.getElementById("divprofile2").style.display = "none";
+    document.getElementById("imgdevlopper").style.display = "none";
+    document.getElementById("profilename").style.display = "none";
+    document.getElementById("moroccoflag").style.display = "none";
+    document.getElementById("hobies").style.display = "none";
+    document.getElementById("TotalServers").style.display = "none";
+    document.getElementById("TotalServers2").style.display = "none";
+    document.getElementById("TotalMembers").style.display = "none";
+    document.getElementById("TotalMembers2").style.display = "none";
+}
+
+function funcinfo() {
+    document.getElementById("texthome").style.display = "none";
+    document.getElementById("textcop").style.display = "none";
+    document.getElementById("imgdis2").style.display = "none";
+    document.getElementById("buttondis22").style.display = "none";
+    document.getElementById("divspaidro").style.display = "none";
+    document.getElementById("imgspaidro").style.display = "none";
+    document.getElementById("imoowner").style.display = "none";
+    document.getElementById("spaibut").style.display = "none";
+    /*Loven block*/
+    document.getElementById("divloven").style.display = "none";
+    document.getElementById("imgloven").style.display = "none";
+    document.getElementById("imcoowner").style.display = "none";
+    document.getElementById("lovenbut").style.display = "none";
+    /*karlos block*/
+    document.getElementById("divkarlos").style.display = "none";
+    document.getElementById("imgkarlos").style.display = "none";
+    document.getElementById("imcoowner2").style.display = "none";
+    document.getElementById("karlosbut").style.display = "none";
+    /*oreo block*/
+    document.getElementById("divoreo").style.display = "none";
+    document.getElementById("imgoreo").style.display = "none";
+    document.getElementById("imcoowner3").style.display = "none";
+    document.getElementById("oreobut").style.display = "none";
+    document.getElementById("titleinformation").style.display = "block";
+    document.getElementById("informationtext").style.display = "block";
+    /**  ========================== */
+    document.getElementById("divprofile1").style.display = "none";
+    document.getElementById("divprofile2").style.display = "none";
+    document.getElementById("imgdevlopper").style.display = "none";
+    document.getElementById("profilename").style.display = "none";
+    document.getElementById("moroccoflag").style.display = "none";
+    document.getElementById("hobies").style.display = "none";
+    document.getElementById("TotalServers").style.display = "none";
+    document.getElementById("TotalServers2").style.display = "none";
+    document.getElementById("TotalMembers").style.display = "none";
+    document.getElementById("TotalMembers2").style.display = "none";
+}
+
+function devfunc() {
+    document.getElementById("texthome").style.display = "none";
+    document.getElementById("textcop").style.display = "none";
+    document.getElementById("imgdis2").style.display = "none";
+    document.getElementById("buttondis22").style.display = "none";
+    document.getElementById("divspaidro").style.display = "none";
+    document.getElementById("imgspaidro").style.display = "none";
+    document.getElementById("imoowner").style.display = "none";
+    document.getElementById("spaibut").style.display = "none";
+    /*Loven block*/
+    document.getElementById("divloven").style.display = "none";
+    document.getElementById("imgloven").style.display = "none";
+    document.getElementById("imcoowner").style.display = "none";
+    document.getElementById("lovenbut").style.display = "none";
+    /*karlos block*/
+    document.getElementById("divkarlos").style.display = "none";
+    document.getElementById("imgkarlos").style.display = "none";
+    document.getElementById("imcoowner2").style.display = "none";
+    document.getElementById("karlosbut").style.display = "none";
+    /*oreo block*/
+    document.getElementById("divoreo").style.display = "none";
+    document.getElementById("imgoreo").style.display = "none";
+    document.getElementById("imcoowner3").style.display = "none";
+    document.getElementById("oreobut").style.display = "none";
+    document.getElementById("titleinformation").style.display = "none";
+    document.getElementById("informationtext").style.display = "none";
+    /**  ========================== */
+    document.getElementById("divprofile1").style.display = "block";
+    document.getElementById("divprofile2").style.display = "block";
+    document.getElementById("imgdevlopper").style.display = "block";
+    document.getElementById("profilename").style.display = "block";
+    document.getElementById("moroccoflag").style.display = "block";
+    document.getElementById("hobies").style.display = "block";
+    document.getElementById("TotalServers").style.display = "block";
+    document.getElementById("TotalServers2").style.display = "block";
+    document.getElementById("TotalMembers").style.display = "block";
+    document.getElementById("TotalMembers2").style.display = "block";
+}
+
+function funchome() {
+    document.getElementById("divprofile1").style.display = "none";
+    document.getElementById("divprofile2").style.display = "none";
+    document.getElementById("imgdevlopper").style.display = "none";
+    document.getElementById("profilename").style.display = "none";
+    document.getElementById("moroccoflag").style.display = "none";
+    document.getElementById("hobies").style.display = "none";
+    document.getElementById("TotalServers").style.display = "none";
+    document.getElementById("TotalServers2").style.display = "none";
+    document.getElementById("TotalMembers").style.display = "none";
+    document.getElementById("TotalMembers2").style.display = "none";
+    document.getElementById("texthome").style.display = "block";
+    document.getElementById("textcop").style.display = "block";
+    document.getElementById("imgdis2").style.display = "none";
+    document.getElementById("buttondis22").style.display = "none";
+    /*spaidro block*/
+    document.getElementById("divspaidro").style.display = "none";
+    document.getElementById("imgspaidro").style.display = "none";
+    document.getElementById("imoowner").style.display = "none";
+    document.getElementById("spaibut").style.display = "none";
+    /*Loven block*/
+    document.getElementById("divloven").style.display = "none";
+    document.getElementById("imgloven").style.display = "none";
+    document.getElementById("imcoowner").style.display = "none";
+    document.getElementById("lovenbut").style.display = "none";
+    /*karlos block*/
+    document.getElementById("divkarlos").style.display = "none";
+    document.getElementById("imgkarlos").style.display = "none";
+    document.getElementById("imcoowner2").style.display = "none";
+    document.getElementById("karlosbut").style.display = "none";
+    /*oreo block*/
+    document.getElementById("divoreo").style.display = "none";
+    document.getElementById("imgoreo").style.display = "none";
+    document.getElementById("imcoowner3").style.display = "none";
+    document.getElementById("oreobut").style.display = "none";
+    document.getElementById("titleinformation").style.display = "none";
+    document.getElementById("informationtext").style.display = "none";
+}
+
+
+function changecolor() {
+    document.getElementById("bodyhomepage").style.backgroundColor = "#D8E3E7";
+    document.getElementById("textcop").style.color = "black";
+    document.getElementById("texthome").style.color = "black";
+    document.getElementById("titleinformation").style.color = "black";
+    document.getElementById("informationtext").style.color = "black";
+    document.getElementById("buttondis22").style.color = "black";
+    document.getElementById("topnav").style.backgroundColor = "white";
+    document.getElementById("divspaidro").style.backgroundColor = "white";
+    document.getElementById("divloven").style.backgroundColor = "white";
+    document.getElementById("divkarlos").style.backgroundColor = "white";
+    document.getElementById("divoreo").style.backgroundColor = "white";
+    document.getElementById("spaibut").style.color = "black";
+    document.getElementById("lovenbut").style.color = "black";
+    document.getElementById("karlosbut").style.color = "black";
+    document.getElementById("oreobut").style.color = "black";
+}
+
+function changecolor2() {
+    location.reload();
+}
